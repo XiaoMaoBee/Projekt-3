2b10bddb0029ced03db32fa01a70224ead29a5cd
Oldrich Lapka <lapka.oldrich@gmail.com>
2024-03-11 13:55:35 +0100

    rozpracováno pro kontrolu

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+// Proměnné pro menu manipulaci 
 const menuIcon = document.querySelector('.menu-icon')
 const menuList = document.querySelector('nav')
-
 const hamburgerIcon = document.querySelector('.fa-solid')   
 
-
+// Menu manipulace 
 menuIcon.addEventListener('click', () => {
     console.log('kliknutí');
     if(hamburgerIcon.classList[1] === 'fa-bars') {
@@ -16,4 +16,12 @@ menuIcon.addEventListener('click', () => {
         hamburgerIcon.classList.add('fa-bars')
         menuList.style.display = 'none'
     }
+})
+
+// Button nahoru 
+
+const btnUp = document.querySelector('.fa-angles-up');
+
+btnUp.addEventListener('click', () => {
+    
 })
