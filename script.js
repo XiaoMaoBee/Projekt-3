70dc5608e3a7c24aa50703dd62a90ba1ef30d5d2
Oldrich Lapka <lapka.oldrich@gmail.com>
2024-03-25 18:12:09 +0100

    validace hesel přidána, dále udělat dark-light mode

--- script.js
+++ script.js
@@ -24,3 +24,46 @@ const btnUp = document.querySelector('.fa-angles-up');
 btnUp.addEventListener('click', () => {
     
 })
+
+// Verifikace hesla
+const pass1 = document.querySelector(".pass1")
+const pass2 = document.querySelector(".pass2")
+const passChecker = document.querySelector(".pass-checker")
+
+pass1.addEventListener("input", () => {
+    const pass1Value = pass1.value
+    const pass2Value = pass2.value
+    if (pass1Value == pass2Value) {
+        passChecker.textContent = "Hesla jsou shodná"
+        passChecker.classList.add("valid")
+        passChecker.classList.remove("invalid")
+    } else {
+        passChecker.textContent = "Hesla nejsou shodná!"
+        passChecker.classList.add("invalid")
+        passChecker.classList.remove("valid")
+    }
+
+    if(pass1Value == "" && pass2Value == "") {
+        passChecker.textContent = ""
+    }
+})
+
+
+    pass2.addEventListener("input", () => {
+        const pass1Value = pass1.value
+        const pass2Value = pass2.value
+        if (pass1Value == pass2Value) {
+            passChecker.textContent = "Hesla jsou shodná"
+            passChecker.classList.add("valid")
+            passChecker.classList.remove("invalid")
+        } else {
+            passChecker.textContent = "Hesla nejsou shodná!"
+            passChecker.classList.add("invalid")
+            passChecker.classList.remove("valid")
+        }
+        
+        if (pass1Value == "" && pass2Value == "") {
+            passChecker.textContent = ""
+            
+        }
+})
